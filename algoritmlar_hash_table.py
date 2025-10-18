38c48622541194b2e3226ba2368647cfb6d54384
Kamolddin05 <kamoliddinraximjonov774@gmail.com>
2025-10-18 13:45:52 +0500

    algorithms

---
+++ algoritmlar_hash_table.py
@@ -0,0 +1,323 @@
+''' 14/10/2025 '''
+
+# table = [None] * 10
+
+# def hash_func(key):
+#     return sum(ord(c) for c in key) % 10
+
+
+# def insert(key, value):
+#     index = hash_func(key)
+#     table[index] = value
+
+
+# def get(key):
+#     index = hash_func(key)
+#     return table[index]
+
+
+# insert("apple", "red")
+# insert("banan", "yellow")
+
+# print(get("apple"))
+# print(get("banan"))
+
+
+
+
+
+
+# table = [None] * 10
+
+# def hesh(key):
+#     return sum(ord(c) for c in key) % 10
+
+# def insert(key, value):
+#     index = hesh(key)
+#     table[index] = value
+
+# def get(key):
+#     index = hesh(key)
+#     return table[index]
+
+# insert("apple","red")
+# insert("banana","yellow")
+
+# print(get("apple"))
+# print(get("banana"))
+
+
+# table = [None] * 10
+
+# def hesh(key):
+#     total = 0
+#     for c in key:
+#         total += ord(c)
+#     return total % 10
+
+
+# def insert(key, value):
+#     index = hesh(key)
+#     table[index] = value
+
+
+# def get(key):
+#     index = hesh(key)
+#     return table[index]
+
+# insert("apple", "red")
+# insert("banana", "yellow")
+
+# print(get("apple"))
+# print(get("banana"))
+
+
+
+
+
+
+
+# table = [None] * 10
+
+# def hesh(key):
+#     total = 0
+#     for c in key:
+#         total += ord(c)
+#     return total % 10
+    
+# def insert(key, value):
+#     index = hesh(key)
+#     table[index] = value
+
+
+# def get(key):
+#     index = hesh(key)
+#     return table[index]
+
+
+# insert("apple", "red")
+# insert("banana", "yellow")
+
+# print(get("apple"))
+# print(get("banana"))
+    
+
+
+
+
+# table = [None] * 10
+
+# def hesh_func(key):
+#     total = 0
+#     for c in key:
+#         total += ord(c)
+#     return total % 10
+
+# def insert(key, value):
+#     index = hesh_func(key)
+#     table[index] = value
+
+# def get(key):
+#     index = hesh_func(key)
+#     return table[index]
+
+# insert("apple", "red")
+# insert("banana", "yellow")
+
+# print(get("apple"))
+# print(get("banana"))
+
+
+
+# table = [None] * 10
+
+# def hesh_func(key):
+#     total = 0
+#     for c in key:
+#         total += ord(c)
+#     return total % 10
+
+# def insert(key, value):
+#     index = hesh_func(key)
+#     table[index] = value
+
+# def get(key):
+#     index = hesh_func(key)
+#     return table[index]
+
+# insert("apple", "red")
+# insert("banana", "yellow")
+
+# print(get("apple"))
+# print(get("banana"))
+
+
+
+
+# table = [None] * 10
+
+# def hesh_func(key):
+#     total = 0
+#     for c in key:
+#         total += ord(c)
+#     return total % 10
+
+# def insert(key, value):
+#     index = hesh_func(key)
+#     table[index] = value
+
+# def get(key):
+#     index = hesh_func(key)
+#     return table[index]
+
+
+# insert("apple", "red")
+# insert("banana", "yellow")
+
+# print(get("apple"))
+# print(get("banana"))
+
+
+
+
+
+# table = [None] * 10
+
+# def hesh_func(key):
+#     total = 0
+#     for c in key:
+#         total += ord(c)
+#     return total % 10
+
+
+# def insert(key, value):
+#     index = hesh_func(key)
+#     table[index] = value
+
+
+# def get(key):
+#     index = hesh_func(key)
+#     return table[index]
+
+# insert("apple", "red")
+# insert("banana", "yellow")
+
+# print(get("apple"))
+# print(get("banana"))
+
+
+
+
+# m1
+
+# table = [None] * 10
+
+# def hesh_func(key):
+#     total = 0
+#     for  c in key:
+#         total += ord(c)
+#     return total %  10
+
+
+# def insert(key, value):
+#     index = hesh_func(key)
+#     table[index] = value
+
+
+# def get(key):
+#     index = hesh_func(key)
+#     return table[index]
+
+# insert("apple", 'red')
+# insert("banana", "yellow")
+
+# print(get("apple"))
+# print(get("banana"))
+
+
+
+# m2
+
+# table = [None] * 10
+
+# def hesh_func(key):
+#     total = 0
+#     for c in key:
+#         total += ord(c)
+#     return total % 10
+
+
+# def insert(key, value):
+#     index = hesh_func(key)
+#     table[index] = value
+
+
+# def get(key):
+#     index = hesh_func(key)
+#     return table[index]
+
+# insert("apple", "red")
+# insert("banana", "yellow")
+# insert("cherry", "derk red")
+
+# print(get("apple"))
+# print(get("banana"))
+# print(get("cherry"))
+
+
+
+
+# m3
+
+# table = [None] * 10
+
+# def hesh_func(key):
+#     total = 0
+#     for c in key:
+#         total += ord(c)
+#     return total % 10
+
+
+# def insert(key, value):
+#     index = hesh_func(key)
+#     table[index] = value
+
+
+# def get(key):
+#     index = hesh_func(key)
+#     return table[index]
+
+# insert("apple", "red")
+# insert("apple", "green")
+
+# print(get("apple"))
+
+
+
+
+# m4
+
+# table = [None] * 10
+
+# def hesh_func(key):
+#     total = 0
+#     for c in key:
+#         total += ord(c)
+#     return total % 10
+
+
+# def insert(key, value):
+#     index = hesh_func(key)
+#     table[index] = value
+
+# def get(key):
+#     index = hesh_func(key)
+#     return table[index]
+
+# insert("apple", "red")
+# insert("banana", "yellow")
+# insert("mango", "orange")
+
+# print(get("apple"))
+# print(get("banana"))
+# print(get("mango"))
